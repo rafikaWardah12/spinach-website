e3bedc6f14e66f699e29102019fcf144a47312dd
rafikaWardah12 <rafika.wardah12@gmail.com>
2024-12-08 21:13:34 +0700

    fix: Hot Fixing UI

--- src/app/ui/main.tsx
+++ src/app/ui/main.tsx
@@ -27,28 +27,32 @@ const Main = ({
           <div className="relative">
             {children}
             { (
-              <div className="absolute z-50 bg-slate-300 opacity-40 top-0 bottom-0 left-0 right-0 items-center flex flex-row justify-center">
-                {/* <svg
-                  className="animate-spin h-20 w-20 text-current"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  />
-                  <path
-                    className="opacity-75"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    fill="currentColor"
-                  />
-                </svg> */}
-              </div>
+              <div className="absolute z-50"> </div>
+              //FOR LOAD PAGE
+            // <div className="absolute z-50 bg-slate-300 top-0 bottom-0 left-0 right-0 items-center flex flex-row justify-center">
+            // </div>
+              // <div className="absolute z-50 bg-slate-300 opacity-40 top-0 bottom-0 left-0 right-0 items-center flex flex-row justify-center">
+              //   {/* <svg
+              //     className="animate-spin h-20 w-20 text-current"
+              //     fill="none"
+              //     viewBox="0 0 24 24"
+              //     xmlns="http://www.w3.org/2000/svg"
+              //   >
+              //     <circle
+              //       className="opacity-25"
+              //       cx="12"
+              //       cy="12"
+              //       r="10"
+              //       stroke="currentColor"
+              //       strokeWidth="4"
+              //     />
+              //     <path
+              //       className="opacity-75"
+              //       d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              //       fill="currentColor"
+              //     />
+              //   </svg> */}
+              // </div>
             )}
           </div>
           <ToastContainer
